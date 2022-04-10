ee5e6068ed7efc0f616e43fc2080014847f98666
yash verma <officialyash23@gmail.com>
2022-04-10 14:00:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let loadingText=document.querySelector(".loading-text")
+let bg=document.querySelector(".bg")
+
+let load=0;
+
+let int=setInterval(blurring,30)
+function blurring() {
+    load++;
+    if(load>99)
+    {
+        clearInterval(int);
+    }
+    loadingText.innerText=`${load}%`
+    loadingText.style.opacity=scale(load,0,100,1,0)
+    bg.style.filter=`blur(${scale(load,0,100,30,0)}px)`
+}
+
+//scale function converta the range of number for  exapmle in out case it will set opacity 1 to 0 but our numbers are from 1 to 100
+const scale=(num,in_min,in_max,out_min,out_max)=>{
+    return (num-in_min)*(out_max-out_min)/(in_max-in_min)+out_min;
+}
